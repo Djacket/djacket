b3c3929167cfb61da36383e841b30aeb288cf69a
Moeen Zamani <moeenzdev@gmail.com>
2017-08-04 04:17:57 +0430

    moved downloading stock avatars to the bottom of build entrypoint as it could interfere with below tasks.

--- docker-entrypoint-build.sh
+++ docker-entrypoint-build.sh
@@ -6,22 +6,6 @@ npm link gulp;
 # GulpJS does the compiling of static files.
 gulp --gulpfile ./frontend/gulpfile.js compile;
 
-# We need to create some stock avatars first.
-if [ ! -d "/srv/media/avatars" ]; then
-    mkdir -p "/srv/media/avatars";
-fi
-
-stock_avatars=3
-if [ ! -d "/srv/media/stock" ]; then
-    mkdir -p "/srv/media/stock";
-else
-    if [[ "$(ls /srv/media/stock | wc -l)" != "$stock_avatars" ]]; then
-        for i in $(seq $stock_avatars $END); do
-            curl "https://api.adorable.io/avatars/200/$i.png" > "/srv/media/stock/$i.png";
-        done
-    fi
-fi
-
 # If we are in development mode then we need to run 'makemigrations'
 #   otherwise 'migrate' would suffice.
 if [[ "$DJKR_MODE" == "dev" ]]; then
@@ -37,6 +21,26 @@ python ./backend/manage.py create_admin;
 # If we are in production we have to collect all the required static files.
 if [[ "$DJKR_MODE" == "prod" ]]; then
     python ./backend/manage.py collectstatic --noinput;
+fi
+
+# We need to create some stock avatars first.
+if [ ! -d "/srv/media/avatars" ]; then
+    mkdir -p "/srv/media/avatars";
+fi
+
+# Here we download some stock avatars.
+stock_avatars=3
+if [ ! -d "/srv/media/stock" ]; then
+    mkdir -p "/srv/media/stock";
+    for i in $(seq $stock_avatars $END); do
+        curl -m 5 "https://api.adorable.io/avatars/200/$i.png" > "/srv/media/stock/$i.png";
+    done
+else
+    if [[ "$(ls /srv/media/stock | wc -l)" != "$stock_avatars" ]]; then
+        for i in $(seq $stock_avatars $END); do
+            curl -m 5 "https://api.adorable.io/avatars/200/$i.png" > "/srv/media/stock/$i.png";
+        done
+    fi
 fi
 
 cat /srv/SIGN;
