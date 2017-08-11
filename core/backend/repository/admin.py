ca9491e0752bb5ad35b756ebe9f81936e8237494
Moeen Zamani <moeen.zamani@gmail.com>
2017-08-11 11:45:25 +0430

    created migrations and apps module for repository app due to new Django policies. initial migration file added too.

--- core/backend/repository/admin.py
+++ core/backend/repository/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from repository.models import Repository, RepositoryStar, RepositoryFork
+from repository.models import Repository, RepositoryAccess, RepositoryStar, RepositoryFork
 
 
 admin.site.register(Repository)
 admin.site.register(RepositoryStar)
 admin.site.register(RepositoryFork)
+admin.site.register(RepositoryAccess)
